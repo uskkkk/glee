5036126e9bd888e2c6a45fedfd55b13acd4b0da1
dntjd4562 <dntjd4562@sinsabridge.com>
2022-12-31 22:37:32 +0900

    초기세팅

---
+++ src/main/java/com/glee/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.glee.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+@RequestMapping("/")
+public class Controller {
+
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ web/WEB-INF/views/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: PC
+  Date: 2022-12-31
+  Time: 오후 10:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello , World</h1>
+</body>
+</html>
